dd560cdd0ea11ae494e6a6944251e3eaabefff34
Hsi-Lai Chiang <8507896+garry101cn@user.noreply.gitee.com>
2022-05-19 20:17:44 +0800

    added ui

--- src/components/login.js
+++ src/components/login.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
+import {Button, Input} from 'antd-mobile'
+
+import './login.css'
+import logo from './res/image/login_logo.png'
 
 class Login extends React.Component{
 
+    constructor() {
+        super();
+        this.username = ""
+        this.passwd = ""
+    }
+
     render(){
+
         return (
-            <h2>登录页面</h2>
+            <div className="login-canvas">
+                <img className='login-logo' src={logo} alt='LoginLogo'></img>
+
+                <Input className='input-user' placeholder='请输入用户名' ></Input>
+                <Input className='input-passwd'></Input>
+
+                <Button className='login-button' color="primary">登录</Button>
+                <Button className='register-button'>注册</Button>
+            </div>
         )
     }
 
